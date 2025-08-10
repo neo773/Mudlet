d0b8e972f7855107f5bfbed75d9a8244457d7749
neo773 <neo773@protonmail.com>
2025-08-10 13:01:05 +0530

    disable check

--- CI/deploy-mudlet-for-windows.sh
+++ CI/deploy-mudlet-for-windows.sh
@@ -105,9 +105,10 @@ else
 fi
 
 # Check if we're building from the Mudlet/Mudlet repository and not a fork
-if [[ "${GITHUB_REPO_NAME}" != "Mudlet/Mudlet" ]]; then
-  exit 2
-fi
+# Temporarily disabled for testing
+# if [[ "${GITHUB_REPO_NAME}" != "Mudlet/Mudlet" ]]; then
+#   exit 2
+# fi
 
 GITHUB_WORKSPACE_UNIX_PATH=$(echo "${GITHUB_WORKSPACE}" | sed 's|\\|/|g' | sed 's|D:|/d|g' | sed 's|C:|/c|g')
 PACKAGE_DIR="${GITHUB_WORKSPACE_UNIX_PATH}/package-${MSYSTEM}-release"
